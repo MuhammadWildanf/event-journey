a138957b4f262e2a36099ff6dabf2f2f5b25f810
MuhammadWildanf <wildanf.daftar@gmail.com>
2025-11-17 05:06:39 +0700

    update scan booth

--- controllers/scanController.js
+++ controllers/scanController.js
@@ -6,6 +6,38 @@ export const showScan = (req, res) => {
     res.render("scan", { user });
 };
 
+// export const handleScanResult = async (req, res) => {
+//     try {
+//         const { boothCode } = req.body;
+//         const user = req.session.user;
+
+//         if (!boothCode) {
+//             return res.status(400).send("QR Code tidak valid");
+//         }
+
+//         const userRef = db.ref(`users/${user.id}`);
+//         const snap = await userRef.get();
+//         const userData = snap.exists() ? snap.val() : {};
+
+//         await userRef.child(`booths_visited/${boothCode}`).set(true);
+
+//         const newSnap = await userRef.child("booths_visited").get();
+//         const visitedCount = newSnap.exists() ? Object.keys(newSnap.val()).length : 0;
+
+//         await userRef.update({
+//             visited_count: visitedCount,
+//             reward_ready: visitedCount >= 5,
+//         });
+
+//         // res.redirect(`/booth/${boothCode}`);
+//         res.redirect(redirectTo);
+//     } catch (err) {
+//         console.error("Error handle scan:", err);
+//         res.status(500).send("Internal Server Error");
+//     }
+// };
+
+
 export const handleScanResult = async (req, res) => {
     try {
         const { boothCode } = req.body;
@@ -29,7 +61,17 @@ export const handleScanResult = async (req, res) => {
             reward_ready: visitedCount >= 5,
         });
 
-        res.redirect(`/booth/${boothCode}`);
+        // 🔥 Ambil redirect jika ada
+       let redirectTo = req.body.redirect || `/booth/${boothCode}`;
+
+
+        // safety redirect
+        if (!redirectTo.startsWith('/')) {
+            redirectTo = `/booth/${boothCode}`;
+        }
+
+        res.redirect(redirectTo);
+
     } catch (err) {
         console.error("Error handle scan:", err);
         res.status(500).send("Internal Server Error");
