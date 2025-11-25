a89fe6eb2336507c0a244be2a1fb615d10519fa5
MuhammadWildanf <wildanf.daftar@gmail.com>
2025-11-26 06:42:57 +0700

    update today

--- utils/date.js
+++ utils/date.js
@@ -1,4 +1,8 @@
 export const getToday = () => {
-    const d = new Date();
-    return d.toISOString().split("T")[0]; // "2025-11-17"
+    const d = new Date(); // waktu lokal server (WIB jika server di Jakarta)
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, "0"); // bulan 0-based
+    const day = String(d.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`; // format "YYYY-MM-DD"
 };
